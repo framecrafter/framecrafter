7549bd8bfa2fa97ad98821c53c680e5b04e9b3b8
Drew Lytle <aml3ed@virginia.edu>
2023-05-18 11:19:29 -0400

    Add vitest

---
+++ apps/demo/src/App.test.tsx
@@ -0,0 +1,5 @@
+import App from "./App";
+
+test("App should render", () => {
+  expect(<App />).not.toBeNull();
+});
--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -1,7 +1,13 @@
+/// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+  },
 });
