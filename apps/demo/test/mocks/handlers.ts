64a965c4fa9b4a9ade43fd6d61ed8c3489ef7703
Drew Lytle <aml3ed@virginia.edu>
2023-05-24 17:22:45 -0400

    setup msw handlers

--- apps/demo/test/mocks/handlers.ts
+++ apps/demo/test/mocks/handlers.ts
@@ -1,37 +1,23 @@
 // src/mocks/handlers.js
 import { rest } from "msw";
+import { readFile } from "fs/promises";
+import { resolve } from "path";
 
 export const handlers = [
-  rest.get("http://www.mywebsite.com/horse-in-motion", (req, res, ctx) => {
-    return res(
-      // Respond with a 200 status code
-      ctx.status(200)
-    );
-  }),
+  rest.get(
+    "http://www.mywebsite.com/horse-in-motion",
+    async (req, res, ctx) => {
+      // Read the image from the file system using the "fs" module.
+      const videoBuffer = await readFile(
+        resolve(__dirname, "../../public/horse-in-motion.mp4")
+      );
+
+      return res(
+        ctx.set("Content-Length", videoBuffer.byteLength.toString()),
+        ctx.set("Content-Type", "video/mp4"),
+        // Respond with the "ArrayBuffer".
+        ctx.body(videoBuffer)
+      );
+    }
+  ),
 ];
-
-// function toArrayBuffer(buffer: Buffer) {
-//   const arrayBuffer = new ArrayBuffer(buffer.length);
-//   const view = new Uint8Array(arrayBuffer);
-//   for (let i = 0; i < buffer.length; ++i) view[i] = buffer[i];
-
-//   return arrayBuffer;
-// }
-// class ResponseFake {
-//   constructor(public buffer: Buffer) {}
-
-//   async json() {
-//     return JSON.parse(this.buffer.toString());
-//   }
-//   async text() {
-//     return this.buffer.toString();
-//   }
-//   async arrayBuffer() {
-//     return toArrayBuffer(this.buffer);
-//   }
-// }
-
-// export async function fileFetch(path: string) {
-//   const buffer = await readFile(resolve(__dirname, path));
-//   return new ResponseFake(buffer);
-// }
